f2529560f720a6cd84294be77988dc7164747743
Kalpadith <ikalpadith@gmail.com>
2024-06-17 11:08:28 +0530

    DataTable component created

---
+++ datatable-project/src/components/DataTable.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Comments DataTable</h1>
+        <table>
+            <tbody>
+                <tr v-for="comment in paginatedComments" :key="comment.id">
+                    <td>{{ comment.id }}</td>
+                    <td>{{ comment.email }}</td>
+                    <td>{{ comment.name }}</td>
+                    <td>{{ comment.body }}</td>
+                    <td>
+                        <button @click="editComment(comment)">Edit</button>
+                        <button @click="removeComment(comment.id)">Remove</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DataTable',
+    }
+</script>
+
+<style src="../styles/dataTable.css"></style>
